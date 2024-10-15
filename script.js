997303d922ecb16dc733332884731577bf67dfe4
Ashinlathika <ashinlathika5@gmail.com>
2024-10-15 13:53:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.signup-btn').addEventListener('click', () => {
+    alert('Sign-up functionality coming soon!');
+});
+
+document.querySelector('.create-playlist-btn').addEventListener('click', () => {
+    alert('Playlist creation feature coming soon!');
+});
